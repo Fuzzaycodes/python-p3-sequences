0fe4b0835736e6edab4bdd38ab3a8e81a0dd6dc5
Fuzzay <samomwerif2@gmail.com>
2024-09-10 15:05:03 +0300

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])  
+        return
+    elif length < 0:
+        print("Length must be a positive integer.")
+        return
+    
+    
+    fibonacci_sequence = [0, 1]
+    
+    
+    for i in range(2, length):
+        next_value = fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]
+        fibonacci_sequence.append(next_value)
+    
+    
+    if length == 1:
+        fibonacci_sequence = [0]
+    
+
+    print(fibonacci_sequence)
+
